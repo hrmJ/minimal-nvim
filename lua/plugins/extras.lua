c816b9e2be1a9f9b0a05672c5596dc171ab2e4c9
Juho Härme <juho.harme@valmetpartners.com>
2025-11-25 09:36:00 +0200

    indentation plugins

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -30,7 +30,7 @@ return {
 			--
 			-- require("mini.pick").setup({})
 			-- require("mini.extra").setup()
-			-- require("mini.indentscope").setup()
+			require("mini.indentscope").setup()
 			-- require("mini.surround").setup({
 			-- 	mappings = {
 			-- 		add = "ys", -- Add surrounding in Normal and Visual modes
@@ -48,7 +48,28 @@ return {
 			-- })
 
 			vim.api.nvim_set_keymap("n", "<leader>p", ":Pick ", { noremap = true, silent = true })
-			-- A CHANGE HERE
+		end,
+	},
+	{
+		"stevearc/aerial.nvim",
+		lazy = false,
+		opts = {},
+		-- Optional dependencies
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("aerial").setup({
+				-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+				on_attach = function(bufnr)
+					-- Jump forwards/backwards with '{' and '}'
+					vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+					vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+				end,
+			})
+			-- You probably also want to set a keymap to toggle aerial
+			vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
 		end,
 	},
 }
